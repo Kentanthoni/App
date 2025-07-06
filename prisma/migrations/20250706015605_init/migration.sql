2d75cf54f8f74366e07585ee6f453c83b4c8172d
Kent Anthoni Román García <122479824+Kentanthoni@users.noreply.github.com>
2025-07-05 23:54:24 -0400

    Delete prisma directory

--- prisma/migrations/20250706015605_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Recipient" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "email" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Recipient_email_key" ON "Recipient"("email");
